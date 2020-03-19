07309226a2b3bd7b5d3e2aa48f22e19d91c11764
Theophile-Kango <fadhili.kango@gmail.com>
2020-03-19 13:08:22 +0200

    adjustement on my corona_info.rb function

--- lita_corona_info/lib/lita/handlers/corona_info.rb
+++ lita_corona_info/lib/lita/handlers/corona_info.rb
@@ -4,17 +4,27 @@ require_relative './scraper'
 module Lita
   module Handlers
     class CoronaInfo < Handler
-      reg = Scraper.new
-      reg.start
-      exp = "/"+reg.get_regex.to_s+"/i"
-      exp = Regexp.new exp
-      route(exp, :get_info)
+      
+      route("china", :get_info)
 
       def get_info response
         scraper = Scraper.new
         scraper.start
-        scraper.get_info(response)
-       end
+        scraper.sort_list.compact.each do |element|
+          if element[0]["Country"].downcase.strip == name.downcase.strip
+              response.response.reply("\nCountry : #{element[0]["Country"]}\n
+              total cases : #{element[1]["Total cases"]}\n
+              new cases : #{element[2]["new cases"]}\n
+              total death : #{element[3]["Total deaths"]}\n
+              new deaths : #{element[4]["New death"]}\n
+              total recovered : #{element[5]["Total recovered"]}\n
+              active cases : #{element[6]["Active Cases"]}\n
+              serious cirtical : #{element[7]["Serious Critical"]}\n
+              total cases/1M pop : #{element[8]["Total cases/1M pop"]} ")
+              break
+          end
+        end
+      end
 
       Lita.register_handler(self)
     end
